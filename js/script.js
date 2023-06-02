0f23af59bff61f912eec5770d9a3b9a0c83d1dac
Dragon3463 <59585297+Dragon3463@users.noreply.github.com>
2023-06-02 18:34:01 -0500

    added_files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Do not change this code
+
+$(document).ready(function(){
+  slideShow();
+});
+
+function slideShow() {
+  var current = $('#photos .show');
+  var next = current.next().length ? current.next() : current.siblings().first();
+  
+  current.hide().removeClass('show');
+  next.fadeIn().addClass('show');
+  
+  setTimeout(slideShow, 5000);
+}
